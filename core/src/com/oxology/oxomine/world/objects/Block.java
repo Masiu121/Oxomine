c6104ea7a920389282bc2181f01c7e8ee45762a4
Masiu121 <maks.kapolka@gmail.com>
2022-01-05 19:25:57 +0100

    Added Block

---
+++ core/src/com/oxology/oxomine/world/objects/Block.java
@@ -0,0 +1,38 @@
+package com.oxology.oxomine.world.objects;
+
+import com.oxology.oxomine.world.Location;
+import com.oxology.oxomine.world.World;
+
+public class Block {
+    World world;
+    Location location;
+
+    int x;
+    int y;
+    int z;
+
+    public Block(Location location) {
+        this.location = location;
+        this.world = this.location.getWorld();
+
+        this.x = (int) location.getX();
+        this.y = (int) location.getY();
+        this.z = (int) location.getZ();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
