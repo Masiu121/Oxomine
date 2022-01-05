dc66e23faa4e794cf1aa6ff2e0f6f8d1cf551737
Masiu121 <maks.kapolka@gmail.com>
2022-01-05 19:25:44 +0100

    Added Entity

---
+++ core/src/com/oxology/oxomine/entites/Entity.java
@@ -0,0 +1,9 @@
+package com.oxology.oxomine.entites;
+
+import com.oxology.oxomine.world.Location;
+import com.oxology.oxomine.world.World;
+
+public class Entity {
+    World world;
+    Location location;
+}
