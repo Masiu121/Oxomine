cd7bef7e28cc22349b0361f441f3231de56fc48e
Masiu121 <maks.kapolka@gmail.com>
2022-01-06 14:52:23 +0100

    Added seed based Random object

--- core/src/com/oxology/oxomine/world/World.java
+++ core/src/com/oxology/oxomine/world/World.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class World {
     private final int smothness = 10;
-    private final int factor = 10;
+    private final int factor = 5;
 
     private String name;
     private int seed;
@@ -16,31 +16,42 @@ public class World {
     private List<Chunk> chunks;
 
     PerlinNoise perlin;
+    Random random;
 
     public World(String name, int seed) {
         this.name = name;
         this.seed = seed;
-        perlin = new PerlinNoise(seed);
+
         chunks = new ArrayList<>();
+
+        perlin = new PerlinNoise(seed);
+        random = new Random(seed);
     }
 
     public World(String name) {
         this.name = name;
         this.seed = new Random().nextInt();
-        perlin = new PerlinNoise(seed);
+
         chunks = new ArrayList<>();
+
+        perlin = new PerlinNoise(seed);
+        random = new Random(seed);
     }
 
-    public Chunk generateChunk(int x, int y) {
+    public void generateChunk(int x, int y) {
         PerlinNoise perlin = new PerlinNoise(seed);
         Chunk chunk = new Chunk(new Location(this, (float) x*Chunk.CHUNK_SIZE, (float) y*Chunk.CHUNK_SIZE, 0));
         for(int i = 0; i < Chunk.CHUNK_SIZE; i++) {
-            float j = factor* perlin.generate(((float) i)/smothness);
+            float j = factor*perlin.generate(((float) i)/smothness);
             for(int k = 0; k < j; k++) {
                 chunk.getBlock(k, i, 0).setType(Material.STONE);
                 chunk.getBlock(k, i, 1).setType(Material.STONE);
             }
         }
-        return chunk;
+        chunks.add(chunk);
+    }
+
+    public String getName() {
+        return name;
     }
 }
