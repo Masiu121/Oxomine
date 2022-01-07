abdae72aa2235d0bd9f4324796e4745f68c5d1cc
Masiu121 <maks.kapolka@gmail.com>
2022-01-07 03:53:59 +0100

    Added materials

--- core/src/com/oxology/oxomine/world/objects/Material.java
+++ core/src/com/oxology/oxomine/world/objects/Material.java
@@ -1,6 +1,25 @@
 package com.oxology.oxomine.world.objects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 public enum Material {
-    AIR,
-    STONE;
+    AIR(),
+    BEDROCK(new Texture(Gdx.files.internal("Bedrock00.png"))),
+    STONE(new Texture(Gdx.files.internal("Stone00.png"))),
+    GRASS(new Texture(Gdx.files.internal("Grass00.png"))),
+    DIRT(new Texture(Gdx.files.internal("Dirt00.png"))),
+    WATER_TOP(new Texture(Gdx.files.internal("Water00.png"))),
+    WATER(new Texture(Gdx.files.internal("Water01.png")));
+
+    Texture texture;
+    Material(Texture texture) {
+        this.texture = texture;
+    }
+
+    Material() {}
+
+    public Texture getTexture() {
+        return texture;
+    }
 }
